f35dd7a145c06c9cda13403d400f6b8373198777
gamal2001 <kmhamak@gmail.com>
2023-10-15 07:27:26 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header">
+      <img src="Troll Face.svg" alt="Troll face" />
+      <h2>Meme Generator</h2>
+      <h4>React Course - Project 3</h4>
+    </header>
+  );
+}
+
+export default Header;
